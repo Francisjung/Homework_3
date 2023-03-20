8f50e4f9e06ec3f6d4dbea117105607ceb636396
Francis Jung <francisjung17@gmail.com>
2023-03-20 03:44:17 -0500

    worked on js

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ var conditions = [];
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  alert(password);
   passwordText.value = password;
 
 }
 function promtUser(){
   var isValid = false;
   while(!isValid){
-    var preCheckLength = prompt("Please enter a password length (minimum 8 characters)");
+    var preCheckLength = prompt("Please enter a password length (minimum 8 characters/ maximum 128)");
     if(Number.isInteger(Number.parseInt(preCheckLength))&& preCheckLength>=8 &&preCheckLength<=128){
       passLength = preCheckLength;
       isValid = true;
@@ -30,11 +30,14 @@ function promtUser(){
       hasNumber = confirm("Include numbers?");
       hasSpecial = confirm("Include special characters?");
       conditions = [hasLower, hasUpper, hasNumber, hasSpecial];
+      console.log("promptUser has successfully run!");
+      console.log("Password length is "+passLength);
   }
 
 function generatePassword(){
   promtUser();
   var passwordString ="";
+
   for(var i=0;i<passLength;i++){
     var selected= false;
     var currentCondition;
@@ -44,48 +47,61 @@ function generatePassword(){
       if(conditions[randomNum]==true){
         selected =true;
         currentCondition = randomNum;
+        console.log("Condition "+randomNum+" selected!")
       }
     }
 
       switch (currentCondition){
         case 0:
           passwordString+= generateLowerChar();
-          return;
+          console.log(passwordString);
+          break;
         case 1:
           passwordString+= generateUpperChar();
-          return;
+          console.log(passwordString);
+          break;
         case 2:
           passwordString+= generateRandomNumber();
-          return;
+          console.log(passwordString);
+          break;
         case 3:
           passwordString+= generateSpecialChar();
-          return;
+          console.log(passwordString);
+          break;
     }
+    console.log("GeneratePassword has successfully run "+i+" times!");
   }
+
+  console.log("generatePassowrd has completed running!")
   return passwordString;
 }
 
 function generateLowerChar(){
   var selector = Math.floor(Math.random() * 26);
   var letters = "abcdefghijklmnopqrstuvwzyx"
+  console.log("generateLowerChar has successfully run! Char generated: "+letters.charAt(selector));
   return letters.charAt(selector);
 }
 
 function generateUpperChar(){
   var returnChar = "";
   returnChar = generateLowerChar();
+  console.log("generateLowerChar has successfully run! Char generated: "+ returnChar.toUpperCase());
   return returnChar.toUpperCase();
 }
 
 function generateRandomNumber(){
-  return Math.floor(Math.random() * 10);
+  var randomNum = Math.floor(Math.random() * 10);
+  console.log("generateRandomNumber has successfully run! Char generated: "+ randomNum);
+  return randomNum;
 }
 
 function generateSpecialChar(){
   var selector = Math.floor(Math.random() * 18);
   var letters = "!@#$%^&*()<>_+|\?/"
+  console.log("generateSpecialChar has successfully run! Char generated: "+letters.charAt(selector));
   return letters.charAt(selector);
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
